552504e228709e3223ca00f87f73fc1e5ee4c814
Albin Johansson <albinoscar99@hotmail.com>
2025-08-16 17:54:10 +0200

    Log configuration info

--- tests/safely_tests.main.cpp
+++ tests/safely_tests.main.cpp
@@ -1,9 +1,26 @@
 // Copyright (C) 2025 Albin Johansson
 
+#include <iostream>
+
 #include <gtest/gtest.h>
+#include <safely/predef.hpp>
 
 auto main(int argc, char* argv[]) -> int
 {
+  std::cout << "__cplusplus: " << __cplusplus << '\n';
+
+#ifdef SAFELY_NO_INTRINSICS
+  std::cout << "SAFELY_NO_INTRINSICS is defined\n";
+#endif
+
+  std::cout << "SAFELY_HAS_STDCKDINT: " << SAFELY_HAS_STDCKDINT << '\n';
+
+  std::cout << "SAFELY_HAS_BUILTIN_ADD_OVERFLOW: "
+            << SAFELY_HAS_BUILTIN_ADD_OVERFLOW << '\n';
+
+  std::cout << "SAFELY_HAS_MSVC_OVERFLOW_INTRINSICS: "
+            << SAFELY_HAS_MSVC_OVERFLOW_INTRINSICS << '\n';
+
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
