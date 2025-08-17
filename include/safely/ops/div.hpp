319ea68a3319ffdca4b98fed674df3fe74b1b194
Albin Johansson <albinoscar99@hotmail.com>
2025-08-18 01:41:09 +0200

    Simplify division implementation

--- include/safely/ops/div.hpp
+++ include/safely/ops/div.hpp
@@ -4,7 +4,6 @@
 
 #include <limits>
 #include <optional>
-#include <type_traits>
 
 #include <safely/detail/traits.hpp>
 #include <safely/detail/unchecked.hpp>
@@ -13,7 +12,7 @@
 namespace safely {
 namespace detail {
 
-template <typename T, std::enable_if_t<is_integer_v<T>, int> = 0>
+template <typename T, integer_concept_t<T> = 0>
 [[nodiscard]] constexpr auto div_check_overflow(const T lhs,
                                                 const T rhs) noexcept -> bool
 {
@@ -31,12 +30,14 @@ template <typename T, std::enable_if_t<is_integer_v<T>, int> = 0>
 
 /// Performs checked integer division.
 ///
+/// \tparam T An integer type.
+///
 /// \param[in] lhs The numerator.
 /// \param[in] rhs The denominator.
 ///
 /// \return
 /// The quotient if successful; an empty optional otherwise.
-template <typename T, std::enable_if_t<detail::is_integer_v<T>, int> = 0>
+template <typename T, detail::integer_concept_t<T> = 0>
 [[nodiscard]] constexpr auto div(const T lhs, const T rhs) noexcept
     -> std::optional<T>
 {
