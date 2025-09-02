ed3cfd3aaf2be7528812aaaf667b72c19d3cc0f0
Albin Johansson <albinoscar99@hotmail.com>
2025-09-02 07:27:26 +0200

    Tweak addition APIs

--- include/safely/ops/add.hpp
+++ include/safely/ops/add.hpp
@@ -11,67 +11,50 @@
 #include <safely/predef.hpp>
 
 #if SAFELY_HAS_STDCKDINT
-  #include <stdbool.h>
+  #include <stdbool.h>  // NOLINT(*-deprecated-headers)
   #include <stdckdint.h>
 #endif
 
 namespace safely {
 namespace detail {
 
-// See SEI CERT C Coding Standard INT32-C.
-template <typename T, signed_integer_concept_t<T> = 0>
-[[nodiscard]] constexpr auto generic_add_check_overflow(const T lhs,
-                                                        const T rhs) noexcept
-    -> bool
+template <typename T, integer_concept_t<T> = 0>
+[[nodiscard]] constexpr auto check_add_overflow_sw(const T lhs,
+                                                   const T rhs) noexcept -> bool
 {
   constexpr auto t_min = std::numeric_limits<T>::min();
   constexpr auto t_max = std::numeric_limits<T>::max();
 
-  if (rhs > 0 && lhs > sub_unchecked(t_max, rhs)) SAFELY_ATTR_UNLIKELY {
-    return true;
+  if constexpr (is_signed_integer_v<T>) {
+    return (rhs > 0 && lhs > sub_unchecked(t_max, rhs)) ||
+           (rhs < 0 && lhs < sub_unchecked(t_min, rhs));
   }
-
-  if (rhs < 0 && lhs < sub_unchecked(t_min, rhs)) SAFELY_ATTR_UNLIKELY {
-    return true;
+  else {
+    return lhs > sub_unchecked(t_max, rhs);
   }
-
-  return false;
-}
-
-// See SEI CERT C Coding Standard INT30-C.
-template <typename T, unsigned_integer_concept_t<T> = 0>
-[[nodiscard]] constexpr auto generic_add_check_overflow(const T lhs,
-                                                        const T rhs) noexcept
-    -> bool
-{
-  constexpr auto t_max = std::numeric_limits<T>::max();
-  return lhs > sub_unchecked(t_max, rhs);
 }
 
 template <typename T, integer_concept_t<T> = 0>
-[[nodiscard]] constexpr auto generic_add(const T lhs, const T rhs) noexcept
+[[nodiscard]] constexpr auto add_sw(const T lhs, const T rhs) noexcept
     -> std::optional<T>
 {
-  if (generic_add_check_overflow(lhs, rhs)) SAFELY_ATTR_UNLIKELY {
+  if (check_add_overflow_sw(lhs, rhs)) SAFELY_ATTR_UNLIKELY {
     return std::nullopt;
   }
 
   return add_unchecked(lhs, rhs);
 }
 
-template <typename T, signed_integer_concept_t<T> = 0>
-[[nodiscard]] constexpr auto generic_add_wrap(const T lhs, const T rhs) noexcept
-    -> T
+template <typename T, integer_concept_t<T> = 0>
+[[nodiscard]] constexpr auto add_wrap_sw(const T lhs, const T rhs) noexcept -> T
 {
-  // Note, this static_cast is implementation defined.
-  return static_cast<T>(add_unchecked(to_unsigned(lhs), to_unsigned(rhs)));
-}
-
-template <typename T, unsigned_integer_concept_t<T> = 0>
-[[nodiscard]] constexpr auto generic_add_wrap(const T lhs, const T rhs) noexcept
-    -> T
-{
-  return add_unchecked(lhs, rhs);
+  if constexpr (is_signed_integer_v<T>) {
+    // Note, this static_cast is implementation defined.
+    return static_cast<T>(add_unchecked(to_unsigned(lhs), to_unsigned(rhs)));
+  }
+  else {
+    return add_unchecked(lhs, rhs);
+  }
 }
 
 }  // namespace detail
@@ -107,7 +90,7 @@ template <typename T, detail::integer_concept_t<T> = 0>
     result = sum;
   }
 #else
-  result = detail::generic_add(lhs, rhs);
+  result = detail::add_sw(lhs, rhs);
 #endif
 
   return result;
@@ -121,7 +104,7 @@ template <typename T, detail::integer_concept_t<T> = 0>
 /// \param[in] rhs The second term.
 ///
 /// \return
-/// The (potentially wrapped) sum of the terms.
+/// The wrapped sum.
 template <typename T, detail::integer_concept_t<T> = 0>
 [[nodiscard]] constexpr auto add_wrap(const T lhs, const T rhs) noexcept -> T
 {
@@ -134,7 +117,7 @@ template <typename T, detail::integer_concept_t<T> = 0>
 #elif SAFELY_HAS_MSVC_OVERFLOW_INTRINSICS
   (void) detail::msvc_add_overflow(lhs, rhs, sum);
 #else
-  sum = detail::generic_add_wrap(lhs, rhs);
+  sum = detail::add_wrap_sw(lhs, rhs);
 #endif
 
   return sum;
@@ -148,22 +131,20 @@ template <typename T, detail::integer_concept_t<T> = 0>
 /// \param[in] rhs The second term.
 ///
 /// \return
-/// The (potentially saturated) sum of the terms.
+/// The saturated sum.
 template <typename T, detail::integer_concept_t<T> = 0>
 [[nodiscard]] constexpr auto add_sat(const T lhs, const T rhs) noexcept -> T
 {
-  constexpr auto t_min = std::numeric_limits<T>::min();
-  constexpr auto t_max = std::numeric_limits<T>::max();
-
-  if (const auto sum = add(lhs, rhs)) {
+  if (const auto sum = add(lhs, rhs)) SAFELY_ATTR_LIKELY {
     return *sum;
   }
 
   if constexpr (detail::is_unsigned_integer_v<T>) {
-    return t_max;
+    return std::numeric_limits<T>::max();
   }
   else {
-    return lhs < 0 ? t_min : t_max;
+    return lhs < 0 ? std::numeric_limits<T>::min()
+                   : std::numeric_limits<T>::max();
   }
 }
 
