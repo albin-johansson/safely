20df52976c76502d41ffe6a38f49b53c39809448
Albin Johansson <albinoscar99@hotmail.com>
2025-09-09 06:57:39 +0200

    Limit shl to unsigned integers

--- include/safely/ops/shl.hpp
+++ include/safely/ops/shl.hpp
@@ -11,35 +11,25 @@
 
 namespace safely {
 
-
 /// Performs a checked bitwise left shift of an integer.
 ///
 /// Note, this function permits unsigned integer overflow.
 ///
-/// \tparam T An integer type.
+/// \tparam T An unsigned integer type.
 ///
 /// \param[in] lhs The shift target.
 /// \param[in] rhs The shift amount.
 ///
 /// \return
 /// The shifted value if successful; an empty optional otherwise.
-template <typename T, detail::integer_concept_t<T> = 0>
+template <typename T, detail::unsigned_integer_concept_t<T> = 0>
 [[nodiscard]] constexpr auto shl(const T lhs, const T rhs) noexcept
     -> std::optional<T>
 {
   constexpr T t_digits {std::numeric_limits<T>::digits};
-  constexpr auto t_max = std::numeric_limits<T>::max();
 
-  if constexpr (detail::is_signed_integer_v<T>) {
-    if (lhs < 0 || rhs < 0 || rhs >= t_digits || lhs > (t_max >> rhs))
-        SAFELY_ATTR_UNLIKELY {
-      return std::nullopt;
-    }
-  }
-  else {
-    if (rhs >= t_digits) SAFELY_ATTR_UNLIKELY {
-      return std::nullopt;
-    }
+  if (rhs >= t_digits) SAFELY_ATTR_UNLIKELY {
+    return std::nullopt;
   }
 
   return detail::shl_unchecked(lhs, rhs);
